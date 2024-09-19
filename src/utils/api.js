e54e74f05a04107f9c982c1f5b7835f40e6beafb
ndanaka <tanakanaoto632@gmail.com>
2024-09-19 02:58:37 -0400

    update logout function

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,31 +1,11 @@
 import axios from "axios";
 // Create an instance of axios
+
 const api = axios.create({
   baseURL: process.env.REACT_APP_SERVER_ADDRESS,
   headers: {
     "Content-Type": "application/json",
   },
 });
-/*
-  NOTE: intercept any error responses from the api
- and check if the token is no longer valid.
- ie. Token has expired or user is no longer
- authenticated.
- logout the user if the token has expired
-*/
-
-// api.interceptors.response.use(
-//   (res) => res,
-//   (err) => {
-//     // alert("interceptors");
-//     alert(err.response.data.msg);
-//     // alert(err.response.status);
-//     if (err.response.status === 401) {
-//       Logout();
-//       // store.dispatch({ type: LOGOUT });
-//     }
-//     return Promise.reject(err);
-//   }
-// );
 
 export default api;
